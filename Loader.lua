3fd273c7a44e81d62042080d491b2a5424060c41
ReallyUnless <168303363+PelaoTruco@users.noreply.github.com>
2026-02-24 18:32:28 -0500

    Delete Loader.lua

--- Loader.lua
+++
@@ -1,8 +0,0 @@
-local base = "https://raw.githubusercontent.com/PelaoTruco/ZembluHub/main/"
-
-local Style = loadstring(game:HttpGet(base.."Style.lua"))()
-local Functions = loadstring(game:HttpGet(base.."Functions.lua"))()
-local Categories = loadstring(game:HttpGet(base.."Categories.lua"))()
-local Main = loadstring(game:HttpGet(base.."Main.lua"))()
-
-Main(Style, Functions, Categories)
