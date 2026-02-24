fe098f9aa6834ecd120e0b87f253096c927baa80
ReallyUnless <168303363+PelaoTruco@users.noreply.github.com>
2026-02-24 18:27:56 -0500

    Update Style.lua

--- Style.lua
+++ Style.lua
@@ -1,9 +1,10 @@
 local Style = {}
 
-Style.Black = Color3.fromRGB(12,12,16)
+Style.Black = Color3.fromRGB(10,10,15)
 Style.DarkPurple = Color3.fromRGB(25,18,40)
 Style.MidPurple = Color3.fromRGB(35,25,55)
 Style.Neon = Color3.fromRGB(170,70,255)
+
 Style.Font = Enum.Font.Code
 
 return Style
